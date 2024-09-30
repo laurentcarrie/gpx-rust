684a8db0bd3059a92b3f3554912dd2579f383914
Laurent Carrie <laurent.carrie@gmail.com>
2024-09-30 12:26:14 +0200

    model and iomodel

---
+++ src/model.rs
@@ -0,0 +1,19 @@
+pub struct Gpx {
+    pub metadata: Metadata,
+    pub trk: Vec<Trkseg>,
+}
+pub struct Metadata {
+    pub desc: String,
+    pub time: String,
+}
+
+pub struct Trkseg {
+    pub trkpt: Vec<Trkpt>,
+}
+
+pub struct Trkpt {
+    pub lat: f32,
+    pub lon: f32,
+    pub time: String,
+    pub hdop: f32,
+}
